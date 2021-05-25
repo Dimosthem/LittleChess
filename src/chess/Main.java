ef9682f487cbcffe8840bd94f89b7253e300e7bb
Dimosthem <74663884+Dimosthem@users.noreply.github.com>
2021-05-25 18:22:07 +0300

    Add files via upload

---
+++ src/chess/Main.java
@@ -0,0 +1,14 @@
+package chess;
+
+public class Main extends Thread {
+	 Thread m=new Thread(){
+		public void run(){
+			Main_Frame k=new Main_Frame();
+		}
+	};
+	
+	public static void main(String args[]){
+		Main_Frame m = new Main_Frame();
+	
+}
+}
